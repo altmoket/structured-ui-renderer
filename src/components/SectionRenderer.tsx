7382712c8f34c7c956dfa2b0ac942efb489043aa
altmoket <leandro.nunez1457@gmail.com>
2026-04-05 22:03:32 -0300

    refactor: Remove if-else conditions and use an object for render sections

--- src/components/SectionRenderer.tsx
+++ src/components/SectionRenderer.tsx
@@ -1,15 +1,12 @@
-import type { Section } from "../types/sections";
+import type { HighlightSection, ListSection, Section, TextSection } from "../types/sections";
 import { RenderHighlight, RenderList, RenderText, RenderUnknown } from "./renderers";
 
+const renderers: Record<string, (section: Section) => React.ReactNode> = {
+  text: (section: Section) => <RenderText {...(section as TextSection)} />,
+  list: (section: Section) => <RenderList {...(section as ListSection)} />,
+  highlight: (section: Section) => <RenderHighlight {...(section as HighlightSection)} />,
+};
+
 export const SectionRenderer = ({ section }: { section: Section; }) => {
-  if (section.type === "text") {
-    return <RenderText {...section} />;
-  }
-  if (section.type === "list") {
-    return <RenderList {...section} />;
-  }
-  if (section.type === "highlight") {
-    return <RenderHighlight {...section} />;
-  }
-  return <RenderUnknown />;
+  return renderers[section.type]?.(section) || <RenderUnknown />;
 };
